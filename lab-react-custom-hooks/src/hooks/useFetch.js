b76fd7fc3426ecc5a986798fab7803e977bbe19c
Danielestoj <danielestoj@gmail.com>
2026-04-15 15:37:25 +0100

    pasos del 1 al 3

---
+++ lab-react-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+
+function useFetch(url){
+    const [data, useData] = useState(null);
+    const [loading, useLoading] = useState(true);
+    const [error, useError] = useState(null);
+
+    useEffect( () => {
+        const fetchedData = async () => {
+
+            try {
+                useLoading(true);
+                const respuesta = await axios.get(url, { signal });
+                useData(respuesta.data);
+            }catch (error) {
+                useError(error);
+            } finally {
+                useLoading(false);
+            }
+
+        };
+
+    }, [url]);
+
+    fetchedData();
+
+    return [data, loading, error];
+
+}
+
+export default useFetch;
